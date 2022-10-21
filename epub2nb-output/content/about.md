f153c26bb7d955ab5ef89725b7708c50894d358c
jan-martinek <jan-martinek@users.noreply.github.com>
2022-10-21 11:48:49 +0000

    Deploying to docs from @ books-are-next/dasenka-cili-zivot-stenete@b43659d3bc8b99b876bf6fd50a18fc0c3c26184b 🚀

--- epub2nb-output/content/about.md
+++ epub2nb-output/content/about.md
@@ -14,7 +14,7 @@ V MKP 2. opravené vydání
 [^2]: Infighting (angl.) – boj zblízka (např. v boxu). _Pozn. red._  
 [^3]: Gagát – tmavý minerál, drahý kámen; dříve zaměňovaný s jantarem. _Pozn. red._  
 ISBN 978-80-274-2474-0 (webová kniha)  
-V MKP 1. elektronické vydání z 10. 10. 2022.
+V MKP 1. elektronické vydání z 21. 10. 2022.
 
 ***
 
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-dc18091';
+const CACHE = 'cache-b43659d';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
